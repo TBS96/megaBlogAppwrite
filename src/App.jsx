6ee30f080c343381800b186f6ff78efd64681606
TBS96 <9tbs6@proton.me>
2024-11-08 22:04:03 +0530

    setup .env variables with appwrite ; created config.js on root dir. => created and exported config as object having env variables with import.meta
    import and destructure config in App.jsx and show the props in console to check proper env setup
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import './App.css'
+import config from './config/config'
 
 const App = () => {
+
+  const {appwriteUrl, appwriteProjectId, appwriteDatabaseId, appwriteCollectionId, appwriteBucketId} = config
+
+  console.log(appwriteUrl)
+  console.log(appwriteProjectId)
+  console.log(appwriteDatabaseId)
+  console.log(appwriteCollectionId)
+  console.log(appwriteBucketId)
+
   return (
     <div>App</div>
   )
