5965de989e53ab5d77c740d3a87257d7c3ea5c68
TBS96 <9tbs6@proton.me>
2024-11-22 11:07:56 +0530

    import and export components Header, Footer, Logo, LogoutBtn, Container from index.js
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
+import LogoutBtn from './Header/LogoutBtn';
+import Container from './Container/Container';
 
 export {
     Header,
     Footer,
-    Logo
+    Logo,
+    LogoutBtn,
+    Container
 }
