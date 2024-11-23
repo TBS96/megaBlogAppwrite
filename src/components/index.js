2b90ae60dd2f1b64ed5e56ec656ef04b3b07b005
TBS96 <9tbs6@proton.me>
2024-11-23 11:59:05 +0530

    create component Login.jsx
    import React, { useState } from 'react'
    import { Link, useNavigate } from 'react-router-dom'
    import { login as storeLogin } from '../store/authSlice'
    import { Button, Input, Logo } from './index'
    import { useDispatch } from 'react-redux'
    import authService from '../appwrite/auth'
    import { useForm } from 'react-hook-form'
    
    const { register, handleSubmit } = useForm()
    
    const { register, handleSubmit } = useForm()
    
    render: <Input
                                label='Email: '
                                placeholder='Enter your email...'
                                type='email'
                                {...register('email', {
                                    required: true,
                                    validate: {
                                        matchPattern: (value) => /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) || 'Email address must be a valid address',
                                    }
                                })}
                            />
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,17 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from './Header/LogoutBtn';
 import Container from './Container/Container';
+import Button from './Button';
+import Input from './Input';
+import Login from './Login';
 
 export {
     Header,
     Footer,
     Logo,
     LogoutBtn,
-    Container
+    Container,
+    Button,
+    Input,
+    Login
 }
