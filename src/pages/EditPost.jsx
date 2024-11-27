36ccb9d3e9b2665f2c69625e2376171974259e11
TBS96 <9tbs6@proton.me>
2024-11-27 17:59:45 +0530

    create page EditPost
    render and wrap PostForm inside Container
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import { Container, PostForm } from '../components/index'
+import appwriteService from '../appwrite/config'
+import { useNavigate, useParams } from 'react-router-dom';
+
+const EditPost = () => {
+
+    const [post, setPost] = useState([]);
+
+    const { slug } = useParams();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (slug) {
+            appwriteService.getPost(slug)
+            .then((post) => {
+                if (post) {
+                    setPost(post)
+                }
+            })
+            .catch(error => {
+                console.log('Error fetching post:', error);
+            })
+        }
+        else {
+            navigate('/');
+        }
+    }, [slug, navigate]);
+
+    return post ? (
+        <div className='py-8'>
+            <Container>
+                <PostForm />
+            </Container>
+        </div>
+    ) : null
+}
+
+export default EditPost
