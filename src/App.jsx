d2d2a009d51dfd776e1b5da0b46015f63ba4599f
TBS96 <9tbs6@proton.me>
2024-11-27 20:20:28 +0530

    uncomment <Outlet /> in App.jsx
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
       <div className='w-full block'>
         <Header />
         <main>
-          {/* <Outlet /> */}
+          <Outlet />
         </main>
         <Footer />
       </div>
