dfa3cf4376a37fb1c457ac2e69698c1f4a9b10cb
TBS96 <9tbs6@proton.me>
2024-12-05 22:07:38 +0530

    moved getPosts() service into useEffect() and done conditional rendering
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -2,31 +2,38 @@ import React, { useEffect, useState } from 'react'
 import { Container, PostCard } from '../components/index'
 import appwriteService from '../appwrite/config'
 
-function AllPosts () {
+function AllPosts() {
 
     const [posts, setPosts] = useState([]);
 
-    useEffect(() => {}, []);
-
-    appwriteService.getPosts([]).then((posts) => {
-        if (posts) {
-            setPosts(posts.documents)
-        }
-    })
-    // .catch(error => {
-    //     console.log('Error in showing posts:', error);
-    // })
+    useEffect(() => {
+        appwriteService.getPosts([]).then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+        .catch(error => {
+            console.log('Error in showing posts:', error);
+        })
+    }, []);
 
     return (
         <div className='w-full py-8'>
             <Container>
-                <div className='flex flex-wrap'>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
-                        </div>
-                    ))}
-                </div>
+                {posts.length > 0 ? (
+                    <>
+                        <h1 className='text-center text-2xl font-bold'>Posts created: {posts.length}</h1>
+                        <div className='flex flex-wrap'>
+                        {posts.map((post) => (
+                            <div key={post.$id} className='p-2 w-1/4'>
+                                <PostCard {...post} />
+                            </div>
+                        ))}
+                    </div>
+                    </>
+                ) : (
+                    <h1 className='text-center text-2xl font-bold'>No posts created. Number of posts: {posts.length}</h1>
+                )}
             </Container>
         </div>
     )
