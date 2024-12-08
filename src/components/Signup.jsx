dffbff781d6d6c46cb341a19d82ff3028a0ecb0e
TBS96 <9tbs6@proton.me>
2024-12-08 12:16:34 +0530

    revamped component Signup.jsx....
    added checkbox for password show/hide toggler
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,23 +2,23 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
-import authService from '../appwrite/auth';
-import { login } from '../store/authSlice';
-import { Logo, Input, Button } from './index'
+import authService from '../appwrite/auth'
+import { login } from '../store/authSlice'
+import { Button, Input, Logo } from './index'
 
 function Signup () {
 
     const navigate = useNavigate();
 
     const dispatch = useDispatch();
-
+    
     const { register, handleSubmit } = useForm();
-
+    
     const [error, setError] = useState('');
 
-    const create = async (data) => {
-        // console.log(data);
-        // alert(JSON.stringify(data));
+    const [showPass, setShowPass] = useState(false);
+
+    const create = async(data) => {
         setError('');
         try {
             const userData = await authService.createAccount(data);
@@ -34,57 +34,62 @@ function Signup () {
     }
 
     return (
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center w-full px-4 sm:px-0'>
             <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
                 <div className='mb-2 flex justify-center'>
                     <span className='inline-block w-full max-w-[100px]'>
                         <Logo width='100%' />
                     </span>
                 </div>
-
                 <h2 className='text-center text-2xl font-bold leading-tight'>Sign up to create an account</h2>
                 <p className='mt-2 text-center text-base text-black/60'>
                     Already have an account?&nbsp;
-                    <Link to='/login' className='font-medium text-blue-500 transition-all duration-200 hover:underline'>
+                    <Link to='/login' className='font-medium text-primary transition-all duration-200 hover:underline'>
                         Sign In
                     </Link>
                 </p>
-                {error &&
-                    <p className='text-red-600 mt-8 text-center'>{error}</p>
-                }
-                <form onSubmit={handleSubmit(create)}>
+                {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+
+                <form onSubmit={handleSubmit(create)} className='mt-8 form-control'>
                     <div className='space-y-5'>
                         <Input
                             label='Full Name: '
-                            placeholder='Enter your full name...'
-                            // type='text'
+                            type='text'
+                            placeholder='Full Name'
                             {...register('name', {
-                                required: true
+                                required: true,
                             })}
                         />
                         <Input
                             label='Email: '
-                            placeholder='Enter your email...'
+                            placeholder='you@voodoo.com'
                             type='email'
                             {...register('email', {
                                 required: true,
                                 validate: {
-                                    matchPattern: (value) => /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) || 'Email address must be a valid address',
+                                    matchPattern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                        'Email address must be a valid address',
                                 }
                             })}
                         />
-                        <Input
-                            label='Password: '
-                            placeholder='Enter your password...'
-                            type='password'
-                            {...register('password', {
-                                required: true,
-                                // validate: {
-                                //     matchPattern: (value) => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(value) || 'at least 8 characters must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number; Can contain special characters'
-                                // }
-                            })}
-                        />
-                        <Button type='submit' className='w-full'>Create Account</Button>
+                        <div className='flex items-end sm:flex-col gap-2'>
+                            <Input
+                                label='Password: '
+                                placeholder='✪✪✪✪✪✪✪✪✪✪✪'
+                                type={showPass ? 'text' : 'password'}
+                                {...register('password', {
+                                    required: true,
+                                    // validate: {
+                                    //     matchPattern: (value) => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(value) || 'at least 8 characters must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number; Can contain special characters'
+                                    // }
+                                })}
+                            />
+                            <input type='checkbox' className='hidden' id='show' onChange={() => setShowPass(!showPass)} />
+                            <label htmlFor="show" className='btn btn-square btn-outline'>{showPass ? 'hide' : 'show'}</label>
+                        </div>
+                        <Button type='submit' className='w-full hover:bg-blue-700'>
+                            Create Account
+                        </Button>
                     </div>
                 </form>
             </div>
