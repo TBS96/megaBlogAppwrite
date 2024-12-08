68d3887a05a159cd94dd0507e4bef79d7b93bf2d
TBS96 <9tbs6@proton.me>
2024-12-08 17:33:10 +0530

    made page Home.jsx responsive
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ function Home () {
                 <div className='flex flex-wrap'>
                     {
                         posts.map((post) => (
-                            <div key={post.$id} className='p-2 w-1/4'>
+                            <div key={post.$id} className='p-2 sm:w-1/2 md:w-1/4'>
                                 <PostCard {...post} />
                             </div>
                         ))
