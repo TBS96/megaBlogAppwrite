7a96ede890fb4b6ac9910eae50382dff276becd6
TBS96 <9tbs6@proton.me>
2024-12-08 12:19:22 +0530

    text color changed for 'Sign up' Link
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -49,7 +49,7 @@ function Login () {
                 <h2 className='text-center text-2xl font-bold leading-tight'>Sign in to your account</h2>
                 <p className='mt-2 text-center text-base text-black/60'>
                     Don&apos;t have any account?&nbsp;
-                    <Link to='/signup' className='font-medium text-blue-500 transition-all duration-200 hover:underline'>
+                    <Link to='/signup' className='font-medium text-primary transition-all duration-200 hover:underline'>
                         Sign Up
                     </Link>
                 </p>
